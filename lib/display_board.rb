6da4647f008d4d665f96261b7b464f90ea82608a
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2019-12-28 21:09:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -23,11 +23,11 @@ end
   
 # Prints a board with an O in the top left position
 def display_board(board = ["O", " ", " ", " ", " ", " ", " ", " ", " "])
-  puts "  #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "  #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "  #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 
