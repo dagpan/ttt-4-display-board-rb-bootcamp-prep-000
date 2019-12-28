1cb4d66938254de57124d3677e0c65599ddcacf4
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2019-12-28 20:42:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -42,7 +42,10 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts "  #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "  #{board[6]} | #{board[7]} | #{board[8]} "
-end# Prints an empty board
+end
+
+
+# Prints an empty board
 
 def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts "  #{board[0]} | #{board[1]} | #{board[2]} "
