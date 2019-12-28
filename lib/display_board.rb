561c410f7f3e0851e2fccf5dbf360cd5804b02f5
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2019-12-28 20:41:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -23,15 +23,18 @@ def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
 end
 
   
-# Prints an empty board
+# Prints a board with an O in the top left position
 
-def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+def display_board(board = ["O", " ", " ", " ", " ", " ", " ", " ", " "])
   puts "  #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "  #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "  #{board[6]} | #{board[7]} | #{board[8]} "
-end# Prints an empty board
+end
+
+
+# Prints an empty board
 
 def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts "  #{board[0]} | #{board[1]} | #{board[2]} "
