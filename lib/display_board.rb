df2aa145668a71bf571ed7719246ce2292c6d632
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2019-12-28 21:14:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -42,3 +42,11 @@ def display_board(board = ["O", " ", " ", " ", "X", " ", " ", " ", " "])
 end
 
 
+# Prints a board winning with X in the top row
+def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
