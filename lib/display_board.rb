e96f34a4577269be018a765716cb96e38739a5df
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2019-12-28 21:18:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -80,3 +80,23 @@ def display_board(board = [" ", " ", "O", " ", "O", " ", "O", " ", " "])
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+
+# Prints a board filled with Xs
+def display_board(board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+# Prints a board filled with Os
+def display_board(board = [" ", " ", "O", " ", "O", " ", "O", " ", " "])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
